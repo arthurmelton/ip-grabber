414a54cf18372227f3ae50f19423626dde7a98a3
arthurmelton <amtitan@icloud.com>
2021-11-02 13:56:01 -0400

    replit work

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 echo "$(tput setaf 2)Setting up"
 install-pkg "http://archive.ubuntu.com/ubuntu/pool/main/g/glib2.0/libglib2.0-0_2.56.4-0ubuntu0.18.04.8_amd64.deb" &> /dev/null
+cd ..
+rm -rf ip-grabber &> /dev/null
 git clone https://github.com/AMTitan/ip-grabber.git &> /dev/null
 cd ip-grabber
 echo "$(tput setaf 2)Running"
